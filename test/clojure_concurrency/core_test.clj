53189460f9f14925a8c3c60293914e76ff45ca58
Johan Astborg <johan@gecemmo.com>
2013-07-31 22:19:17 +0200

    initial commit

---
+++ test/clojure_concurrency/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-concurrency.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-concurrency.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
